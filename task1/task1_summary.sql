0e935debe0f535d2224948bda378745980a9332d
Ygthnn <yigithan091@gmail.com>
2026-04-10 14:11:58 +0300

    Add task1 solution

--- task1/task1_summary.sql
+++ task1/task1_summary.sql
@@ -1,14 +1,20 @@
 DROP TABLE IF EXISTS summary_by_year;
 
 CREATE TABLE summary_by_year AS
+WITH normalized_prices AS (
+    SELECT
+        publication_year,
+        CASE
+            WHEN price_text LIKE '$%' THEN CAST(SUBSTR(price_text, 2) AS REAL)
+            WHEN price_text LIKE '€%' THEN ROUND(CAST(SUBSTR(price_text, 2) AS REAL) * 1.2, 2)
+            ELSE NULL
+        END AS price_usd
+    FROM books_raw
+)
 SELECT
     publication_year,
     COUNT(*) AS book_count,
-    ROUND(AVG(price_text), 2) AS average_price
-FROM books_raw
+    ROUND(AVG(price_usd), 2) AS average_price_usd
+FROM normalized_prices
 GROUP BY publication_year
 ORDER BY publication_year;
-
-SELECT COUNT(*) AS books_raw_count FROM books_raw;
-SELECT COUNT(*) AS summary_count FROM summary_by_year;
-SELECT * FROM summary_by_year ORDER BY publication_year;
